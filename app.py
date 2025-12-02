492b8c6864513e393551244e0a4f3f83045ada36
sviswas1997 <sviswas1997@gmail.com>
2025-12-02 23:08:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,5 +81,6 @@ def process():
     return send_file(io.BytesIO(pdf), mimetype="application/pdf", as_attachment=True, download_name="control_charts_scrap_qty.pdf")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
